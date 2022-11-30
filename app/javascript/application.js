bd41b3452891d209ce1e606b77b2894ed5e6d74f
girijaasoni <115526987+girijaasoni@users.noreply.github.com>
2022-11-30 08:21:48 -0500

    Pf set up (#14)
    * Installed and configured patterfly and used a button component
    
    * added package-lock.json to gitignore
    
    * changed the button function to one liner

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "@patternfly/react-core/dist/styles/base.css";
+import { Button } from '@patternfly/react-core';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+
+const App = () => <Button variant="primary">PF check</Button>
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+    <App />,
+    document.getElementById('root'),
 );

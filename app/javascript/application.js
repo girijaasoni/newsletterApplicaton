7dfbd220affec422d847586c226d72b396491010
girijaasoni <gisoni@redhat.com>
2022-11-30 07:29:03 -0500

    INstalled and configured patterfly and used a button component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "@patternfly/react-core/dist/styles/base.css";
+import { Button } from '@patternfly/react-core';
+
 
 function App() {
-  return (<h1>Hello World!</h1>);
+    return (
+        <div>
+             <Button variant="primary">PF check</Button>
+        </div>
+       
+    );
 }
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+    <App />,
+    document.getElementById('root'),
 );

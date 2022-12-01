888c79db85e15e2fcb289a879827f6b80fccb825
Archana Kumari <32739028+archanaserver@users.noreply.github.com>
2022-12-01 16:13:59 +0530

    Setup CI of ESlint with Prettier  (#18)
    * Setup CI of ESlint with Prettier
    
    This PR contains the following changes:
    
    - Installed and configured ESlint with Prettier for the project. Run command- `npx prettier --write app/` to format the `app` directpry and `npx prettier --check .` in CI to make sure that your project stays formatted.
    - Added CI with ESlint
    - Downgrade to use React 16.14.0, same as Foreman [Reference](https://github.com/LiorKGOW/My-Guitar-Store/pull/13)
    
    Resolves #6 Installed and configured CI of ESlint with Prettier
    
    * fix: npm command in eslint.yml file
    
    * fix: npm command in eslint.yml file
    
    * test: eslint ci
    
    * fix: webpack.config.js code style
    
    * added: node-> true env
    
    * update: eslint.yml file

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      files: ['app/javascript/**/*.js']
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "@patternfly/react-core/dist/styles/base.css";
+import '@patternfly/react-core/dist/styles/base.css';
 import { Button } from '@patternfly/react-core';
 
+const App = () => <Button variant="primary">PF check</Button>;
 
-const App = () => <Button variant="primary">PF check</Button>
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));

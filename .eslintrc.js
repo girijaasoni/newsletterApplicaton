910df039bf6d3e0744b26b25914a6156ae6f9ec2
archanaserver <ak31960466@gmail.com>
2022-12-01 11:51:03 +0530

    added: node-> true env

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true
+    jest: true,
+    node: true
   },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
   overrides: [

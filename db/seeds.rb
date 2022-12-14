8d1eb457d508d64d255d25a3198ac5452d929649
archanaserver <ak31960466@gmail.com>
2022-12-14 15:11:06 +0530

    Seeding some database from db/seeds.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.destroy_all
+
+Article.create!([{
+    creator_name: "John Watson",
+    article_title: "Ruby on Rails with PostgreSQL",
+    article_body: "This is the main body of the article."
+},
+{
+    creator_name: "Shrek",
+    article_title: "Title of Shrek article",
+    article_body: "This is the main body of the article."
+},
+{
+    creator_name: "Emma",
+    article_title: "Title of Emmma article",
+    article_body: "This is the main body of the article."
+}])
+
+p "Created #{Article.count} Articles."

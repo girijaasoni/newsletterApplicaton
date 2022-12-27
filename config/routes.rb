a7e874499f081b1545cc521a9d74991aa7605532
Archana Kumari <32739028+archanaserver@users.noreply.github.com>
2022-12-27 18:15:31 +0530

    Setup CRUD Scaffold (#21)
    * Setup CRUD Scaffold
    
    This PR contains the following changes:
    - A new `aricles` view which perform all the crud operations. You can view this folder from `/app/views/articles`
    - Also added the routes `resources :articles` in the `routes.rb` file.
    - Go to `localhost:3000/articles` and you will see the list of the articles, also you can create new articles.
    
    Resolves #11 #2 Setup CRUD scaffold into the project
    
    * update: added few new cops in .rubocop.yml file
    
    * Added `gem 'rubocop-i18n'` in the Gemfile.
    
    * Seeding some database from db/seeds.rb file.
    
    * fix: rubocop rules
    
    * resolved comments
    
    * added: workflow file to test ruby
    
    * update variable names
    
    * update variable names
    
    * fix fixture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "root#index"
+  resources :articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.destroy_all
+
+Article.create!([{
+                  creator: "John Watson",
+                  title: "Ruby on Rails with PostgreSQL",
+                  body: "This is the main body of the article."
+                },
+                 {
+                   creator: "Shrek",
+                   title: "Title of Shrek article",
+                   body: "This is the main body of the article."
+                 },
+                 {
+                   creator: "Emma",
+                   title: "Title of Emmma article",
+                   body: "This is the main body of the article."
+                 }])
+
+Rails.logger.debug { "Created #{Article.count} Articles." }
